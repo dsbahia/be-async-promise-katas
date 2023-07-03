07ab7ba93f0657cae23581f8e7626420ba721784
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-07-03 23:08:55 +0100

    promise-katas-1 completed

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -25,17 +25,27 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = () => {
+  return fetch("food").then((response) => {
+    return response.data;
+  });
+};
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = () => {
+  return fetch("cats").then((response) => Object.values(response.data.cats).sort());
+};
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = () => {
+  return fetch("dogs").then((response) => {
+    
+  });
+};
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return {
